0938ae3c224a276743a238cf2771d2fc5de1fa05
Ahmed <Ahmed.nader1994@gmail.com>
2022-06-11 10:24:59 -0700

    get real dates

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -3,13 +3,19 @@ package com.udacity.asteroidradar.main
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.database.getDatabase
 import com.udacity.asteroidradar.repository.AsteroidsRepository
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
+    private lateinit var today: String
+    private lateinit var lastDay: String
     private val database = getDatabase(application)
     private val asteroidsRepository = AsteroidsRepository(database)
 
@@ -17,8 +23,9 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
      * init{} is called immediately when this ViewModel is created.
      */
     init {
+        getDates()
         viewModelScope.launch {
-            asteroidsRepository.refreshAsteroids("2015-09-07","2015-09-08",application.resources.getString(
+            asteroidsRepository.refreshAsteroids(today,lastDay,application.resources.getString(
                 R.string.API_KEY))
         }
     }
@@ -26,4 +33,15 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     val asteroids = asteroidsRepository.asteroids
 
+
+    private fun getDates() {
+
+        val calendar = Calendar.getInstance()
+            val currentTime = calendar.time
+            val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
+            today = dateFormat.format(currentTime)
+            calendar.add(Calendar.DAY_OF_YEAR, 7)
+            val lastTime = calendar.time
+            lastDay = dateFormat.format(lastTime)
+    }
 }
