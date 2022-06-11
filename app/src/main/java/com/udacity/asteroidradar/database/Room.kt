bd186ae5d9ae5c629af0f5baa70055c502efead5
Ahmed <Ahmed.nader1994@gmail.com>
2022-06-11 09:44:45 -0700

    room db implementation

---
+++ app/src/main/java/com/udacity/asteroidradar/database/Room.kt
@@ -0,0 +1,35 @@
+package com.udacity.asteroidradar.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.udacity.asteroidradar.domain.Asteroid
+
+@Dao
+interface AsteroidDao {
+    @Query("select * from asteroid")
+    fun getAsteroids(): LiveData<List<AsteroidDao>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg asteroid: Asteroid)
+}
+
+
+
+@Database(entities = [Asteroid::class], version = 1)
+abstract class AsteroidDatabase : RoomDatabase() {
+    abstract val asteroidDao: AsteroidDao
+}
+
+private lateinit var INSTANCE: AsteroidDatabase
+
+fun getDatabase(context: Context): AsteroidDatabase {
+    synchronized(AsteroidDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                AsteroidDatabase::class.java,
+                "asteroidsDB").build()
+        }
+    }
+    return INSTANCE
+}
